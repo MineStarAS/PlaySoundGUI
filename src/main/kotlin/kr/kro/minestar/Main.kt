d1df5acb661d4e13c9e7cd9e6831edaabfbab1e2
MineStar <band1019@naver.com>
2021-08-31 10:14:11 +0900

    # prefix edit

--- src/main/kotlin/kr/kro/minestar/Main.kt
+++ src/main/kotlin/kr/kro/minestar/Main.kt
@@ -4,7 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     companion object {
-        const val prefix = "§f§7[§9MineStar's Plugin§7] §f"
+        const val prefix = "§f§7[§9MineStar's Plugin§7]§f"
     }
 
     override fun onEnable() {

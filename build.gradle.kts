3bad24d5230791f5fad2f2618cbe8c027065ae48
MineStar <band1019@naver.com>
2022-01-05 16:52:05 +0900

    #

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    kotlin("jvm") version "1.5.30"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("jvm") version "1.6.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("org.jetbrains.dokka") version "1.5.0"
     `maven-publish`
 }
 
-group = "kr.kro.minestar.playsoundgui"
+group = "kr.kro.minestar"
 version = "17.1"
+val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.17.1 - vanilla\\plugins")
 
 repositories {
     mavenCentral()
@@ -13,28 +15,42 @@ repositories {
     maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") {
         name = "sonatype-oss-snapshots"
     }
-    maven(url = "https://jitpack.io/")
+    maven("https://repo.projecttl.net/repository/maven-public/")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("net.kyori:adventure-api:4.7.0")
+    compileOnly("net.kyori:adventure-api:4.9.3")
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 
     //project_TL
 
     //MineStar
+    implementation("kr.kro.minestar:Utility-API:1.5.12")
 }
 
-tasks{
+tasks {
+    compileKotlin{
+        kotlinOptions.jvmTarget = "16"
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+    }
     processResources {
         filesMatching("*.yml") {
             expand(project.properties)
         }
     }
+
     create<Jar>("sourcesJar") {
-        archiveClassifier.set("source")
+        archiveClassifier.set("sources")
         from(sourceSets["main"].allSource)
+    }
+
+    create<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+        from("$buildDir/dokka/html")
     }
     shadowJar {
         archiveBaseName.set(project.name)
@@ -44,17 +60,58 @@ tasks{
             // jar file copy
             copy {
                 from(archiveFile)
-                val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.17.1 - vanilla\\plugins")
                 into(if (File(plugins, archiveFileName.get()).exists()) plugins else plugins)
             }
         }
     }
 }
+
 publishing {
     publications {
-        create<MavenPublication>(project.name) {
-            artifact(tasks["sourcesJar"])
+        create<MavenPublication>("${rootProject.name}-api") {
             from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            repositories {
+                maven {
+                    name = "MavenCentral"
+                    val releasesRepoUrl = "https://repo.projecttl.net/repository/maven-releases/"
+                    val snapshotsRepoUrl = "https://repo.projecttl.net/repository/maven-snapshots/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+                    credentials.runCatching {
+                        username = project.properties["username"] as String?
+                        password = project.properties["password"] as String?
+                    }
+                }
+
+                pom {
+                    val repository = rootProject.name
+                    name.set(repository)
+                    description.set("This is MineStar's $repository plugin")
+                    url.set("https://github.com/MineStarAS/$repository")
+                    licenses {
+                        license {
+                            name.set("GNU GENERAL PUBLIC LICENSE Version 3")
+                            url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("MineStarAS")
+                            name.set("MineStar")
+                            email.set("band1019@naver.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/MineStarAS/$repository.git")
+                        developerConnection.set("scm:git:https://github.com/MineStarAS/$repository.git")
+                        url.set("https://github.com/MineStarAS/$repository.git")
+                    }
+                }
+            }
         }
     }
 }
+
